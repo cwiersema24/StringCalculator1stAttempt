ecb5894b2faef9989ce2c89b0f72199bbae3e7d2
coraw24@gmail.com <coraw24@gmail.com>
2020-09-15 14:51:43 -0600

    Add project files.

---
+++ StringCalculatorBlank/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StringCalculatorBlank
+{
+    public class CalculatorTests
+    {
+        StringCalculator _calculator;
+        public CalculatorTests()
+        {
+            _calculator = new StringCalculator();
+        }
+        [Fact]
+        public void EmptyStringReturnsZero()
+        {
+            var calculator = new StringCalculator();
+            int answer = calculator.Add("");
+            Assert.Equal(0, answer);
+
+        }
+
+        [Theory]
+        [InlineData ("2",2)]
+        [InlineData("24", 24)]
+        public void StringNumber(string numbers, int expected)
+        {
+            int answer = _calculator.Add(numbers);
+
+            Assert.Equal(expected, answer);
+        }
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("3,4", 7)]
+        public void StringWithCommas(string numbers, int expected)
+        {
+            int answer = _calculator.Add(numbers);
+            Assert.Equal(expected, answer);
+        }
+        [Theory]
+        [InlineData("1,2,3,4,5,6,7,8,9,10,11,12", 78)]
+        public void MuchasNumbres(string numbers, int expected)
+        {
+            int answer = _calculator.Add(numbers);
+            Assert.Equal(expected, answer);
+        }
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("3\n4\n5\n6",18)]
+        public void LineReturn(string numbers, int expected)
+        {
+            int answer = _calculator.Add(numbers);
+            Assert.Equal(expected, answer);
+        }
+
+        [Theory]
+        [InlineData("//;1;3",4)]
+        public void NewSeporator(string numbers, int expected)
+        {
+            int answer = _calculator.Add(numbers);
+            Assert.Equal(expected, answer);
+        }
+    }
+}
---
+++ StringCalculatorBlank/StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorBlank
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if(numbers == "")
+            {
+                return 0;
+            }
+            if (numbers.StartsWith("//"))
+            {
+                var newseporator = numbers[2];
+                var numbers2 = numbers.Remove(0, 2);
+                var parts = numbers2.Split(newseporator);
+                var total = 0;
+                foreach (var part in parts)
+                {
+                    total += int.Parse(part);
+                }
+                return total;
+            }
+            else
+            {
+                return GetTotal(numbers);
+            }
+            
+        }
+        private int GetTotal( string numbers)
+        {
+            var seporators = new List<char> { ',', '\n' };
+            
+            var parts = numbers.Split(seporators.ToArray());
+            var total = 0;
+            foreach (var part in parts)
+            {
+                total += int.Parse(part);
+            }
+            return total;
+        }
+    }
+}
